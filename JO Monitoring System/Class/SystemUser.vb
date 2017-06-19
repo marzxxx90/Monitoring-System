03c54efb51b34529b8e563ffe2c5c19120e87f44
Bladier <misionaellie@gmail.com>
2017-06-19 15:09:51 +0800

    UPDATE

---
+++ JO Monitoring System/Class/SystemUser.vb
@@ -0,0 +1,112 @@
+﻿Public Class SystemUser
+
+    Dim mysql As String = String.Empty
+    Dim tblLogin As String = "tbllogin"
+
+    Private _ID As Integer
+    Public Property ID() As Integer
+        Get
+            Return _ID
+        End Get
+        Set(ByVal value As Integer)
+            _ID = value
+        End Set
+    End Property
+
+    Private _USERNAME As String
+    Public Property USERNAME() As String
+        Get
+            Return _USERNAME
+        End Get
+        Set(ByVal value As String)
+            _USERNAME = value
+        End Set
+    End Property
+
+    Private _USERPASS As String
+    Public Property USERPASS() As String
+        Get
+            Return _USERPASS
+        End Get
+        Set(ByVal value As String)
+            _USERPASS = value
+        End Set
+    End Property
+
+    Private _FIRSTNAME As String
+    Public Property FIRSTNAME() As String
+        Get
+            Return _FIRSTNAME
+        End Get
+        Set(ByVal value As String)
+            _FIRSTNAME = value
+        End Set
+    End Property
+
+
+    Private _MIDDLENAME As String
+    Public Property MIDDLENAME() As String
+        Get
+            Return _MIDDLENAME
+        End Get
+        Set(ByVal value As String)
+            _MIDDLENAME = value
+        End Set
+    End Property
+
+
+    Private _LASTNAME As String
+    Public Property LASTNAME() As String
+        Get
+            Return _LASTNAME
+        End Get
+        Set(ByVal value As String)
+            _LASTNAME = value
+        End Set
+    End Property
+
+    Private _ROLE As String
+    Public Property ROLE() As String
+        Get
+            Return _ROLE
+        End Get
+        Set(ByVal value As String)
+            _ROLE = value
+        End Set
+    End Property
+
+    Private _STATUS As Integer
+    Public Property STATUS() As Integer
+        Get
+            Return _STATUS
+        End Get
+        Set(ByVal value As Integer)
+            _STATUS = value
+        End Set
+    End Property
+
+#Region "FUNCTIONS AND PROCEDURES"
+    Friend Sub CreateADMIN(Optional ByVal pssword As String = "MISDEPT", Optional ByVal username As String = "Admin")
+        mysql = "SELECT * FROM " & tblLogin
+        Dim ds As DataSet = LoadSQL(mysql, tblLogin)
+
+        Dim Fname = "MIS", Lname = "DEPARTMENT", ROLE = "Admin"
+
+        Dim dsNewrow As DataRow
+        dsNewrow = ds.Tables(0).NewRow
+
+        With dsNewrow
+            .Item("USERNAME") = username
+            .Item("USERPASS") = EncryptString(pssword)
+            .Item("FNAME") = Fname
+            .Item("LNAME") = Lname
+            .Item("ROLE") = ROLE
+            .Item("STATUS") = 1
+        End With
+        ds.Tables(0).Rows.Add(dsNewrow)
+        database.SaveEntry(ds)
+        Console.WriteLine("Admin created.")
+    End Sub
+
+#End Region
+End Class
---
+++ JO Monitoring System/Forms/frmLogin.Designer.vb
@@ -0,0 +1,90 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmLogin
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmLogin))
+        Me.txtusername = New System.Windows.Forms.TextBox()
+        Me.txtuserpass = New System.Windows.Forms.TextBox()
+        Me.btnLogin = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtusername
+        '
+        Me.txtusername.Location = New System.Drawing.Point(159, 131)
+        Me.txtusername.Name = "txtusername"
+        Me.txtusername.Size = New System.Drawing.Size(141, 20)
+        Me.txtusername.TabIndex = 0
+        '
+        'txtuserpass
+        '
+        Me.txtuserpass.Location = New System.Drawing.Point(159, 158)
+        Me.txtuserpass.Name = "txtuserpass"
+        Me.txtuserpass.Size = New System.Drawing.Size(141, 20)
+        Me.txtuserpass.TabIndex = 1
+        Me.txtuserpass.UseSystemPasswordChar = True
+        '
+        'btnLogin
+        '
+        Me.btnLogin.BackColor = System.Drawing.Color.Transparent
+        Me.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnLogin.Location = New System.Drawing.Point(168, 201)
+        Me.btnLogin.Name = "btnLogin"
+        Me.btnLogin.Size = New System.Drawing.Size(54, 27)
+        Me.btnLogin.TabIndex = 2
+        Me.btnLogin.UseVisualStyleBackColor = False
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.Color.Transparent
+        Me.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnExit.Location = New System.Drawing.Point(228, 201)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(54, 27)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'frmLogin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(406, 332)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnLogin)
+        Me.Controls.Add(Me.txtuserpass)
+        Me.Controls.Add(Me.txtusername)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "frmLogin"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "frmLogin"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtusername As System.Windows.Forms.TextBox
+    Friend WithEvents txtuserpass As System.Windows.Forms.TextBox
+    Friend WithEvents btnLogin As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+End Class
